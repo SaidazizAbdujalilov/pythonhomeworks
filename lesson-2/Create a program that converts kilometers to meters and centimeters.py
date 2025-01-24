76d4182ff51594ebbda69a30d540265b43144416
SaidazizAbdujalilov <saidazizabdujalilov7@gmail.com>
2025-01-24 18:39:37 +0500

    2

---
+++ lesson-2/Create a program that converts kilometers to meters and centimeters.py
@@ -0,0 +1,5 @@
+km = float(input("Enter the distance in kilometers: "))
+m = km * 1000
+cm = km * 100000
+print(f"{m} m")
+print(f"{cm} cm")
---
+++ lesson-2/Write a Python file that asks for three numbers and outputs the largest and smallest..py
@@ -0,0 +1,8 @@
+print("Write integers:")
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+max = max(a, b, c)
+min = min(a, b, c)
+print(f"Biggest: {max}")
+print(f"Smallest: {min}")
