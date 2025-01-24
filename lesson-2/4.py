54ac4dedb7e146f85617a33518684e19b7135224
SaidazizAbdujalilov <saidazizabdujalilov7@gmail.com>
2025-01-24 19:37:18 +0500

    23

---
+++ lesson-2/4.py
@@ -0,0 +1,7 @@
+A = int(input("You are dividing:")) 
+print("by")
+B = int(input())
+a = A // B
+b = A % B
+print(f"Answer: {a}")
+print(f"Remainder: {b}")
---
+++ lesson-2/6.py
@@ -0,0 +1,3 @@
+A = input("Number: ")
+B = A[len(A)-1]
+print(f"Last digit: {B}")
