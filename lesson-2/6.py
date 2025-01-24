28f3f478d0d9ea61605729e4e5cce647012dc3dd
SaidazizAbdujalilov <saidazizabdujalilov7@gmail.com>
2025-01-24 19:54:00 +0500

    234

--- lesson-2/6.py
+++ lesson-2/6.py
@@ -1,3 +1,11 @@
 A = input("Number: ")
-B = A[len(A)-1]
-print(f"Last digit: {B}")
+if A.isdigit(): 
+    B = A[len(A)-1]
+    print(f"Last digit: {B}")
+else: 
+    A = input("Number: ")
+    if A.isdigit(): 
+       B = A[len(A)-1]
+       print(f"Last digit: {B}")
+    else: 
+        print("Go to hell")
